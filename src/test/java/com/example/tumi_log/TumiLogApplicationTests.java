941747f9966bcc7dade894bbc68c668cf6a9d677
Yuma Nakagome <yuma@MacBook-Air-4.local>
2025-11-25 22:40:06 +0900

    fix: enforce disable MySQL datasource in tests

--- src/test/java/com/example/tumi_log/TumiLogApplicationTests.java
+++ src/test/java/com/example/tumi_log/TumiLogApplicationTests.java
@@ -4,8 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+// SpringBootTestアノテーションを以下のように変更
+// propertiesでデータソースURLを空文字で上書きし、MySQLへの接続を完全に無効化します
+@SpringBootTest(properties = {
+		"spring.datasource.url="
+})
 @ActiveProfiles("test") // testプロファイルを有効化する
-@SpringBootTest
+
 class TumiLogApplicationTests {
 
 	@Test
