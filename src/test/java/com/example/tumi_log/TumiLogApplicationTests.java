430b9ceace4e25933b3ed7e9c321880524315340
Yuma Nakagome <yuma@MacBook-Air-4.local>
2025-11-25 22:47:07 +0900

    refactor: Revert temporary CI-specific datasource exclusion

--- src/test/java/com/example/tumi_log/TumiLogApplicationTests.java
+++ src/test/java/com/example/tumi_log/TumiLogApplicationTests.java
@@ -1,20 +1,11 @@
 package com.example.tumi_log;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-// 以下の2つのAutoConfigurationをテスト時は実行しないように完全に除外する
-@SpringBootTest
-@EnableAutoConfiguration(exclude = {
-		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class
-})
 @ActiveProfiles("test") // testプロファイルを有効化する
-
+@SpringBootTest
 class TumiLogApplicationTests {
 
 	@Test
