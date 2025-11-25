590754f91368d2d14b4816a47088a042ba50989a
Yuma Nakagome <yuma@MacBook-Air-4.local>
2025-11-25 22:42:15 +0900

    fix: disable all DataSource and JPA autoconfiguration in tests

--- src/test/java/com/example/tumi_log/TumiLogApplicationTests.java
+++ src/test/java/com/example/tumi_log/TumiLogApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.tumi_log;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-// SpringBootTestアノテーションを以下のように変更
-// propertiesでデータソースURLを空文字で上書きし、MySQLへの接続を完全に無効化します
-@SpringBootTest(properties = {
-		"spring.datasource.url="
+// 以下の2つのAutoConfigurationをテスト時は実行しないように完全に除外する
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
 })
 @ActiveProfiles("test") // testプロファイルを有効化する
 
