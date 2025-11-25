c7c331aa572d0500ecb9857acc0ea6d87290b817
Yuma Nakagome <yuma@MacBook-Air-4.local>
2025-11-25 22:30:29 +0900

    fix: disable DB connection in CI tests

--- src/test/java/com/example/tumi_log/TumiLogApplicationTests.java
+++ src/test/java/com/example/tumi_log/TumiLogApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.tumi_log;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test") // testプロファイルを有効化する
 @SpringBootTest
 class TumiLogApplicationTests {
 
